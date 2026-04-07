0fa60269562c43f50530f5b8cf6c37c4fa43360b
shitalpawar21 <shitalpawar23950@gmail.com>
2026-04-07 17:40:47 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,47 @@
+var arr = [20 , 'shiv' , 22.4]
+
+//console.log(arr)
+
+// acess the element in aaaray
+ 
+var a = arr[1]
+
+console.log(arr[2])
+
+// replace  element 
+arr[0]='shital'
+
+console.log(arr)
+
+console.log('the length of array is ', arr.length)
+
+
+// inbuit method array
+ var arr2 = [10, 20, 30, 40]
+
+// Pop 
+
+
+var c = arr2.pop()
+console.log(c)
+
+console.log('poped array ', arr2)
+
+//push
+
+arr.push(100)
+
+console.log('push array ',arr2)
+
+//shift method 
+
+var d = arr2.shift()
+console.log(d)
+
+console.log('shifted array', arr2)
+
+//unshift Array
+
+arr2.unshift(102)
+
+console.log('unshifted array', arr2)
